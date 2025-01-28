f54ab07295535f1a251e784a8737f3a48a89cecf
valenjarapatino <143661904+valenjarapatino@users.noreply.github.com>
2025-01-28 17:27:54 -0500

    se crea proyecto

--- controllers/controladorRegistro.js
+++ controllers/controladorRegistro.js
@@ -3,13 +3,10 @@
 let boton=document.getElementById("boton");
 //2. se configura el evento utilizando la funcion addventlistener
 boton.addEventListener("click" ,function(evento){
-  
     evento.preventDefault()
-   
     //rutina parta capturar y validar datos
-
     //paso1:se crea una variable para cada imput del formulario
-let nombre=document.getElementById("nombre").vule;
+let nombre=document.getElementById("nombre").value;
 let descripcion=document.getElementById("descripcion").value;
 let precio=document.getElementById("precio").value;
 let foto =document.getElementById("foto").value;
@@ -25,11 +22,24 @@ let datos ={
 }
 //paso3:enlazar el html con los datos
 let resultado=document.getElementById("texto")
-resultado.textContent=datos
+resultado.textContent=JSON.stringify(datos)
 
+
+if(nombre.length>0 && descripcion.length>0 && precio.length>0 && foto.length>0 && estado.length>0 ){
     Swal.fire({
         title: "Producto registrado!",
         text: "hemos registrado tu productocon exicto",
         icon: "success"
+    });
+}else{
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Why do I have this issue?</a>'
       });
+
+}
+
+    
 });
---
+++ data/datos.js
@@ -0,0 +1,58 @@
+let datos=[
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    },
+    {
+        nombre:"Copa de frutos rojos",
+        descripcion:"3 salsas difentes que bañan un concierto de vainilla en tu paladar",
+        precio:"15000",
+        foto:"../assets/img/productos/producto1.png",
+        estado:true
+    }
+]
